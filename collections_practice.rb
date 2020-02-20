ef8690417a32077bd402a747154cde85f655a19c
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-20 00:59:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort {|first_n, second_n| second_n <=> first_n}
+end
+
+def sort_array_char_count(string)
+  string.sort {|left, right| left.length <=> right.length
+end
