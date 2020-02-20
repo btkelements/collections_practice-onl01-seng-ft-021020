4d4bbf9bdbe05ed188299360794f4f76715ca9bd
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-20 01:13:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,8 +6,21 @@ def sort_array_desc(integers)
   integers.sort {|first_n, second_n| second_n <=> first_n}
 end
 
-def sort_array_char_count(string)
-  string.sort {|left, right| left.length <=> right.length
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length
 end
 
-def swap_elements
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
